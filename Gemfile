612fa7d4a07acc64d83fb5cb2f79857a314982d0
catriona beamish <mani.motamedifr@gmail.com>
2024-01-31 13:44:17 +0100

    SQLite to Post

--- Gemfile
+++ Gemfile
@@ -22,7 +22,8 @@ gem "autoprefixer-rails"
 gem "sassc-rails"
 
 # Use sqlite3 as the database for Active Record
-gem "sqlite3", "~> 1.4"
+# gem "sqlite3", "~> 1.4"
+gem "pg"
 # gem "pg"
 
 # Use the Puma web server [https://github.com/puma/puma]
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,12 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_01_04_183513) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "colors", force: :cascade do |t|
     t.string "name"
-    t.integer "product_id", null: false
+    t.bigint "product_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["product_id"], name: "index_colors_on_product_id"
